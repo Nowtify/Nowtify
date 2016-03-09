87e9d903485fdeb1197cee9c289ef9d659b3aaff
josiekiew <josie.kiew.2013@sis.smu.edu.sg>
2016-03-09 17:57:49 +0800

    front_end_shit

--- app/src/main/java/com/udacity/firebase/nowtify/ui/EntityItem/EntityItemDetailsActivity.java
+++ app/src/main/java/com/udacity/firebase/nowtify/ui/EntityItem/EntityItemDetailsActivity.java
@@ -1,13 +1,16 @@
 package com.udacity.firebase.nowtify.ui.EntityItem;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.koushikdutta.ion.Ion;
 import com.udacity.firebase.nowtify.R;
 import com.udacity.firebase.nowtify.model.EntityItemDetails;
 import com.udacity.firebase.nowtify.ui.BaseActivity;
@@ -22,12 +25,22 @@ public class EntityItemDetailsActivity extends BaseActivity {
     private FirebaseUtils fireBaseUtils = new FirebaseUtils(getParent());
     private Firebase firebaseRef;
     private EntityItemDetails entityItemDetails;
+    private String mEntityParentName;
+    private String mEntityItemDetailsTitle;
+    private String mEntityItemDetailsId;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entity_item_details);
-        
+
+        context = EntityItemDetailsActivity.this;
+
+        //retrieve intent
+        mEntityParentName = getIntent().getStringExtra("entityParentName");
+        mEntityItemDetailsTitle = getIntent().getStringExtra("title");
+        mEntityItemDetailsId = getIntent().getStringExtra("entityItemDetailsId");
         getEntityItemDetailsFromId("adidasGroupOffer");
 
         /**
@@ -58,8 +71,50 @@ public class EntityItemDetailsActivity extends BaseActivity {
     public void setDetails(){
         //edit here to set views
         //use entityItemDetails.[get]
-        TextView textViewListName = (TextView) findViewById(R.id.entity_parent_name);
-        textViewListName.setText();
+        TextView mEntityParentNameView = (TextView) findViewById(R.id.entity_parent_name);
+        mEntityParentNameView.setText(mEntityParentName);
+
+        ImageView mEntityItemImageView = (ImageView) findViewById(R.id.entity_item_image);
+        Ion.with(context)
+                .load("http://dope.sg/wp-content/uploads/2014/03/111.jpg")
+                .withBitmap()
+                .placeholder(R.drawable.icon_add)
+                .error(R.drawable.icon_profile_add)
+                .intoImageView(mEntityItemImageView);
+
+
+        TextView mEntityItemAddressView = (TextView) findViewById(R.id.entity_item_address);
+        mEntityItemAddressView.setText(entityItemDetails.getTitle());
+
+        TextView mEntityItemDetailTitle = (TextView) findViewById(R.id.entity_item_address);
+        mEntityItemAddressView.setText(entityItemDetails.getTitle());
+
+        //Details
+        //Hi Momo, you can insert here:
+
+        TextView mOpeningHours = (TextView) findViewById(R.id.entity_child_opening_hours);
+        mOpeningHours.setText("11am - 11pm");
+
+        TextView mStartDate = (TextView) findViewById(R.id.entity_item_start_date);
+        mStartDate.setText("999");
+
+        TextView mEndDate = (TextView) findViewById(R.id.entity_item_end_date);
+        mEndDate.setText("000");
+
+        //Description
+        TextView mDescription = (TextView) findViewById(R.id.entity_item_description);
+        mDescription.setText("tage 1: Confirm ideal date/time, change the colour status of the timetable doc when they confirm (link below)\n" +
+                "\n" +
+                "Stage 2: If unable to make it, put on hold and let us know, we will wait till all companies in the same day to reply. If happened that same day got company cant make it, we will propose a switch between companies within the same day only.\n" +
+                "\n" +
+                "Stage 3a: If really cannot, we will slot in extra/new companies within the location that recently updated their status (to fill the empty slot).\n" +
+                "            \"");
+
+        //Terms and Condition
+        TextView mTermsAndCondition = (TextView) findViewById(R.id.entity_item_tnc);
+        mTermsAndCondition.setText("have a scenario where, after logging in through a login page");
+
+
 
     }
 }
